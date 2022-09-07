2516f23da1183cb12f10238b97ab12fe18c9d58f
sophiev <sophie.viera@umce.cl>
2022-09-06 22:18:48 -0300

    cambio de header

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@ window.addEventListener('load', (event) => {
     $('#footer').load('/parts/footer.html');
 
     var form = document.getElementById('formContacto');
+    var nombres = document.getElementById('nombres').value
+    var apellidos = document.getElementById('apellidos').value
+    var edad = document.getElementById('edad').value
+    var correo = document.getElementById('correo').value
+    var sexo = document.getElementById('sexo').value
+    var motivo = document.getElementById('motivo').value
 
     form.addEventListener('click', function tomaDeDatos(e) {
         e.preventDefault();
 
-        var nombres = document.getElementById('nombres').value
-        var apellidos = document.getElementById('apellidos').value
-        var edad = document.getElementById('edad').value
-        var correo = document.getElementById('correo').value
-        var sexo = document.getElementById('sexo').value
-        var motivo = document.getElementById('motivo').value
+        nombres.addEventListener('keyup', function(){
+            if(nombres == ''){
+                var alertaNombre = document.getElementById('alertaNombre');
 
-        
+                alert.innerText('hola');
+            }
+        })
     });
 
 });
