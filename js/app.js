6d78b80a5e883f0e278d39247314864659cd35fe
sophiev <sophie.viera@umce.cl>
2022-09-06 22:14:10 -0300

    arreglo de header

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+window.addEventListener('load', (event) => {
+
     $('#header').load('/parts/header.html');
     $('#footer').load('/parts/footer.html');
+
+    var form = document.getElementById('formContacto');
+
+    form.addEventListener('click', function tomaDeDatos(e) {
+        e.preventDefault();
+
+        var nombres = document.getElementById('nombres').value
+        var apellidos = document.getElementById('apellidos').value
+        var edad = document.getElementById('edad').value
+        var correo = document.getElementById('correo').value
+        var sexo = document.getElementById('sexo').value
+        var motivo = document.getElementById('motivo').value
+
+        
+    });
+
 });
